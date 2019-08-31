7b25e152293120bc4dfa831cc1389238150a9e63
zycdojar <40334209+zycdojar@users.noreply.github.com>
2019-08-31 19:55:02 +0800

    commit

---
+++ src/main/java/com/zycdojar/Functions/Reference.java
@@ -0,0 +1,10 @@
+package com.zycdojar.Functions;
+
+/**
+ * Created by jy on 2019/8/31.
+ */
+public class Reference {
+    public static final String NAME = "redxdttt";
+    public static final String MODID = "redxdttt";
+    public static final String VERSION = "des";
+}
--- src/main/java/com/zycdojar/main.java
+++ src/main/java/com/zycdojar/main.java
@@ -2,34 +2,37 @@ package com.zycdojar;
 
 import com.zycdojar.Proxys.Common;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.Instance;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
+import static net.minecraftforge.fml.common.Mod.*;
+
 /**
  * Created by jy on 2019/8/31.
  */
 @Mod(modid = "redxdttt",name = "redxdttt",version = "des")
 public class main {
-    @Mod.Instance
-    public static main instancel;
+    @Instance
+    public static main instance = new main();
     @SidedProxy(clientSide = "com.zycdojar.Proxys.Client",serverSide = "com.zycdojar.Proxys.Common")
     public static Common proxy;
     public static Logger log;
-    @Mod.EventHandler
+    @EventHandler
     public static void preInit(FMLPreInitializationEvent event){
         log.info("Execute1");
         log=event.getModLog();
         proxy.preInit(event);
     }
-    @Mod.EventHandler
+    @EventHandler
     public static void init(FMLInitializationEvent event){
         log.info("Execute2");
         proxy.init(event);
     }
-    @Mod.EventHandler
+    @EventHandler
     public static void postInit(FMLPostInitializationEvent event){
         proxy.postInit(event);
     }
