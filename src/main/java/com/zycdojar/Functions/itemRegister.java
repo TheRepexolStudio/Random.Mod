825d939bbf4ce6ca0a2228344f7dcaedef091342
zycdojar <40334209+zycdojar@users.noreply.github.com>
2019-08-31 20:12:00 +0800

    d

---
+++ src/main/java/com/zycdojar/Functions/itemRegister.java
@@ -0,0 +1,11 @@
+package com.zycdojar.Functions;
+
+import com.zycdojar.Mod.item.ErrorIngot;
+
+/**
+ * Created by jy on 2019/8/31.
+ */
+public class itemRegister {
+    public static ErrorIngot ei = new ErrorIngot("erroringot");
+    public itemRegister(){}
+}
---
+++ src/main/java/com/zycdojar/Mod/item/ErrorIngot.java
@@ -0,0 +1,15 @@
+package com.zycdojar.Mod.item;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+
+/**
+ * Created by jy on 2019/8/31.
+ */
+public class ErrorIngot extends Item {
+    public ErrorIngot(String name){
+        setUnlocalizedName(name);
+        setRegistryName(name);
+        setCreativeTab(CreativeTabs.MATERIALS);
+    }
+}
