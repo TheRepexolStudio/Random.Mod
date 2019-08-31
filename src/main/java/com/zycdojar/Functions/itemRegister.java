176bb0a552da56f47c1a32202dc11cc91db301fe
zycdojar <40334209+zycdojar@users.noreply.github.com>
2019-08-31 20:13:21 +0800

    S

--- src/main/java/com/zycdojar/Functions/itemRegister.java
+++ src/main/java/com/zycdojar/Functions/itemRegister.java
@@ -1,10 +1,12 @@
 package com.zycdojar.Functions;
 
 import com.zycdojar.Mod.item.ErrorIngot;
+import net.minecraftforge.fml.common.Mod;
 
 /**
  * Created by jy on 2019/8/31.
  */
+@Mod.EventBusSubscriber
 public class itemRegister {
     public static ErrorIngot ei = new ErrorIngot("erroringot");
     public itemRegister(){}
