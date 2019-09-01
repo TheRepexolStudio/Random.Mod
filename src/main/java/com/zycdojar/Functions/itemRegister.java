b67f7340c0873f010d790210ef53a9dc5de1cfb8
zycdojar <40334209+zycdojar@users.noreply.github.com>
2019-09-01 12:28:34 +0800

    arw

--- src/main/java/com/zycdojar/Functions/itemRegister.java
+++ src/main/java/com/zycdojar/Functions/itemRegister.java
@@ -1,13 +1,20 @@
 package com.zycdojar.Functions;
 
 import com.zycdojar.Mod.item.ErrorIngot;
-import net.minecraftforge.fml.common.Mod;
+import net.minecraft.item.Item;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+import static net.minecraftforge.fml.common.Mod.*;
 
 /**
  * Created by jy on 2019/8/31.
  */
-@Mod.EventBusSubscriber
+@EventBusSubscriber
 public class itemRegister {
     public static ErrorIngot ei = new ErrorIngot("erroringot");
-    public itemRegister(){}
+    @SubscribeEvent
+    public static void onItemReg(RegistryEvent.Register<Item> event){
+        event.getRegistry().register(ei);
+    }
 }
--- src/main/java/com/zycdojar/inA/Func1.java
+++ src/main/java/com/zycdojar/inA/Func1.java
@@ -4,4 +4,7 @@ package com.zycdojar.inA;
  * Created by jy on 2019/9/1.
  */
 public class Func1 {
+    public static void main(String[] args) {
+        System.out.println("该模组只能在我的世界Forge里使用!");
+    }
 }
